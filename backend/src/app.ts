c4d6d2396e0026348a3be3b2bd8c544f357de976
josephsb <jguku2002@gmail.com>
2023-11-29 14:18:59 -0500

    install backend dependencys and add project structure

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express, { Router } from "express";
+import cors from "cors";
+//import { ALLOWED_ORIGINS } from "../config";
+
+interface Options {
+  port?: number
+  routes: Router
+}
+
+export class Server {
+  public readonly app = express();
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port = 3000, routes } = options;
+
+    this.port = port;
+    this.routes = routes;
+  }
+
+  async start() {
+    // Middlewares
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true })); // use for request x-www-formurlencode
+
+    // use routes
+    this.app.use(this.routes);
+
+
+    this.app.listen(this.port, () => console.log(`Server run on port ${this.port}`))
+  }
+
+}
